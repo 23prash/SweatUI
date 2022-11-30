5a499b326404cff9e483a2862fdea585d78c9a7f
macbook <macbook@macbooks-MacBook-Pro.local>
2022-11-30 18:18:51 +1100

    first commit: add Mistic + define colors.

---
+++ Sources/SweatUI/SweatUI.swift
@@ -0,0 +1,7 @@
+import MisticaCommon
+
+public struct SweatUI {
+    public static func config() {
+        MisticaConfig.brandStyle = .o2Classic
+    }
+}
---
+++ Sources/SweatUI/UIColor+SweatUI.swift
@@ -0,0 +1,348 @@
+import SwiftUI
+import MisticaCommon
+
+public extension UIColor {
+    static var buttonDangerBackground: UIColor {
+        MisticaConfig.currentColors.buttonDangerBackground
+    }
+
+    static var buttonDangerBackgroundSelected: UIColor {
+        MisticaConfig.currentColors.buttonDangerBackgroundSelected
+    }
+
+    static var buttonLinkBackgroundSelected: UIColor {
+        MisticaConfig.currentColors.buttonLinkBackgroundSelected
+    }
+
+    static var buttonLinkBackgroundSelectedInverse: UIColor {
+        MisticaConfig.currentColors.buttonLinkBackgroundSelectedInverse
+    }
+
+    static var buttonPrimaryBackground: UIColor {
+        MisticaConfig.currentColors.buttonPrimaryBackground
+    }
+
+    static var buttonPrimaryBackgroundInverse: UIColor {
+        MisticaConfig.currentColors.buttonPrimaryBackgroundInverse
+    }
+
+    static var buttonPrimaryBackgroundSelected: UIColor {
+        MisticaConfig.currentColors.buttonPrimaryBackgroundSelected
+    }
+
+    static var buttonPrimaryBackgroundSelectedInverse: UIColor {
+        MisticaConfig.currentColors.buttonPrimaryBackgroundSelectedInverse
+    }
+
+    static var buttonSecondaryBackground: UIColor {
+        MisticaConfig.currentColors.buttonSecondaryBackground
+    }
+
+    static var buttonSecondaryBackgroundSelected: UIColor {
+        MisticaConfig.currentColors.buttonSecondaryBackgroundSelected
+    }
+
+    static var buttonSecondaryBorderInverse: UIColor {
+        MisticaConfig.currentColors.buttonSecondaryBorderInverse
+    }
+
+    static var buttonSecondaryBorderSelectedInverse: UIColor {
+        MisticaConfig.currentColors.buttonSecondaryBorderSelectedInverse
+    }
+
+    static var textButtonPrimary: UIColor {
+        MisticaConfig.currentColors.textButtonPrimary
+    }
+
+    static var textButtonPrimaryInverse: UIColor {
+        MisticaConfig.currentColors.textButtonPrimaryInverse
+    }
+
+    static var textButtonPrimaryInverseSelected: UIColor {
+        MisticaConfig.currentColors.textButtonPrimaryInverseSelected
+    }
+
+    static var textButtonSecondary: UIColor {
+        MisticaConfig.currentColors.textButtonSecondary
+    }
+
+    static var textButtonSecondarySelected: UIColor {
+        MisticaConfig.currentColors.textButtonSecondarySelected
+    }
+
+    static var textButtonSecondaryInverse: UIColor {
+        MisticaConfig.currentColors.textButtonSecondaryInverse
+    }
+
+    static var textButtonSecondaryInverseSelected: UIColor {
+        MisticaConfig.currentColors.textButtonSecondaryInverseSelected
+    }
+
+    static var textLink: UIColor {
+        MisticaConfig.currentColors.textLink
+    }
+
+    static var textLinkInverse: UIColor {
+        MisticaConfig.currentColors.textLinkInverse
+    }
+
+    static var textLinkDanger: UIColor {
+        MisticaConfig.currentColors.textLinkDanger
+    }
+
+    static var textLinkSnackbar: UIColor {
+        MisticaConfig.currentColors.textLinkSnackbar
+    }
+
+    // MARK: Backgrounds
+
+    static var appBarBackground: UIColor {
+        MisticaConfig.currentColors.appBarBackground
+    }
+
+    static var background: UIColor {
+        MisticaConfig.currentColors.background
+    }
+
+    static var backgroundContainer: UIColor {
+        MisticaConfig.currentColors.backgroundContainer
+    }
+
+    static var backgroundBrand: UIColor {
+        MisticaConfig.currentColors.backgroundBrand
+    }
+
+    static var backgroundOverlay: UIColor {
+        MisticaConfig.currentColors.backgroundOverlay
+    }
+
+    static var backgroundSkeleton: UIColor {
+        MisticaConfig.currentColors.backgroundSkeleton
+    }
+
+    static var backgroundSkeletonInverse: UIColor {
+        MisticaConfig.currentColors.backgroundSkeletonInverse
+    }
+
+    static var navigationBarBackground: UIColor {
+        MisticaConfig.currentColors.navigationBarBackground
+    }
+
+    static var backgroundAlternative: UIColor {
+        MisticaConfig.currentColors.backgroundAlternative
+    }
+
+    static var backgroundFeedbackBottom: UIColor {
+        MisticaConfig.currentColors.backgroundFeedbackBottom
+    }
+
+    static var skeletonWave: UIColor {
+        MisticaConfig.currentColors.skeletonWave
+    }
+
+    // MARK: BadgeView
+
+    static var badge: UIColor {
+        MisticaConfig.currentColors.badge
+    }
+
+    // MARK: Tab bar
+
+    static var textAppBarSelected: UIColor {
+        MisticaConfig.currentColors.textAppBarSelected
+    }
+
+    static var textAppBar: UIColor {
+        MisticaConfig.currentColors.textAppBar
+    }
+
+    // MARK: Borders
+
+    static var borderLight: UIColor {
+        MisticaConfig.currentColors.borderLight
+    }
+
+    static var border: UIColor {
+        MisticaConfig.currentColors.border
+    }
+
+    static var borderDark: UIColor {
+        MisticaConfig.currentColors.borderDark
+    }
+
+    static var borderSelected: UIColor {
+        MisticaConfig.currentColors.borderSelected
+    }
+
+    // MARK: Feedback
+
+    static var feedbackInfoBackground: UIColor {
+        MisticaConfig.currentColors.feedbackInfoBackground
+    }
+
+    static var feedbackErrorBackground: UIColor {
+        MisticaConfig.currentColors.feedbackErrorBackground
+    }
+
+    // MARK: Controls
+
+    static var control: UIColor {
+        MisticaConfig.currentColors.control
+    }
+
+    static var controlActivated: UIColor {
+        MisticaConfig.currentColors.controlActivated
+    }
+
+    static var controlError: UIColor {
+        MisticaConfig.currentColors.controlError
+    }
+
+    static var loadingBar: UIColor {
+        MisticaConfig.currentColors.loadingBar
+    }
+
+    static var loadingBarBackground: UIColor {
+        MisticaConfig.currentColors.loadingBarBackground
+    }
+
+    // MARK: Dividers
+
+    static var divider: UIColor {
+        MisticaConfig.currentColors.divider
+    }
+
+    static var dividerInverse: UIColor {
+        MisticaConfig.currentColors.dividerInverse
+    }
+
+    static var navigationBarDivider: UIColor {
+        MisticaConfig.currentColors.navigationBarDivider
+    }
+
+    // MARK: Global
+
+    static var brand: UIColor {
+        MisticaConfig.currentColors.brand
+    }
+
+    static var brandHigh: UIColor {
+        MisticaConfig.currentColors.brandHigh
+    }
+
+    static var inverse: UIColor {
+        MisticaConfig.currentColors.inverse
+    }
+
+    static var neutralHigh: UIColor {
+        MisticaConfig.currentColors.neutralHigh
+    }
+
+    static var neutralLow: UIColor {
+        MisticaConfig.currentColors.neutralLow
+    }
+
+    static var neutralMedium: UIColor {
+        MisticaConfig.currentColors.neutralMedium
+    }
+
+    static var promo: UIColor {
+        MisticaConfig.currentColors.promo
+    }
+
+    static var highlight: UIColor {
+        MisticaConfig.currentColors.highlight
+    }
+
+    static var textPrimary: UIColor {
+        MisticaConfig.currentColors.textPrimary
+    }
+
+    static var textPrimaryInverse: UIColor {
+        MisticaConfig.currentColors.textPrimaryInverse
+    }
+
+    static var textSecondary: UIColor {
+        MisticaConfig.currentColors.textSecondary
+    }
+
+    static var textSecondaryInverse: UIColor {
+        MisticaConfig.currentColors.textSecondaryInverse
+    }
+
+    // MARK: States
+
+    static var error: UIColor {
+        MisticaConfig.currentColors.error
+    }
+
+    static var success: UIColor {
+        MisticaConfig.currentColors.success
+    }
+
+    static var warning: UIColor {
+        MisticaConfig.currentColors.warning
+    }
+
+    // MARK: Bars Texts
+
+    static var textNavigationBarPrimary: UIColor {
+        MisticaConfig.currentColors.textNavigationBarPrimary
+    }
+
+    static var textNavigationBarSecondary: UIColor {
+        MisticaConfig.currentColors.textNavigationBarSecondary
+    }
+
+    static var textNavigationSearchBarHint: UIColor {
+        MisticaConfig.currentColors.textNavigationSearchBarHint
+    }
+
+    static var textNavigationSearchBarText: UIColor {
+        MisticaConfig.currentColors.textNavigationSearchBarText
+    }
+
+    // MARK: Gradient
+
+    static var backgroundBrandGradient: [UIColor] {
+        MisticaConfig.currentColors.backgroundBrandGradient.map(\.self)
+    }
+
+    // MARK: Tag
+
+    static var promoLow: UIColor {
+        MisticaConfig.currentColors.promoLow
+    }
+
+    static var brandLow: UIColor {
+        MisticaConfig.currentColors.brandLow
+    }
+
+    static var successLow: UIColor {
+        MisticaConfig.currentColors.successLow
+    }
+
+    static var warningLow: UIColor {
+        MisticaConfig.currentColors.warningLow
+    }
+
+    static var errorLow: UIColor {
+        MisticaConfig.currentColors.errorLow
+    }
+
+    static var promoHigh: UIColor {
+        MisticaConfig.currentColors.promoHigh
+    }
+
+    static var successHigh: UIColor {
+        MisticaConfig.currentColors.successHigh
+    }
+
+    static var warningHigh: UIColor {
+        MisticaConfig.currentColors.warningHigh
+    }
+
+    static var errorHigh: UIColor {
+        MisticaConfig.currentColors.errorHigh
+    }
+}
